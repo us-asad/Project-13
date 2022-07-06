4d60c223c824143f334ad396e28b87d1b55a6a4a
Asad <usasad99@gmail.com>
2022-07-06 13:40:17 +0500

    starter template completed

---
+++ components/index.js
@@ -0,0 +1,2 @@
+export { default as HeaderTop } from "./HeaderTop.jsx";
+export { default as Navbar } from "./Navbar.jsx";
---
+++ containers/Header.jsx
@@ -0,0 +1,10 @@
+import { HeaderTop } from 'components'
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <HeaderTop />
+    </header>
+  )
+}
